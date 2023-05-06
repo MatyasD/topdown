009e04b42286224ba550fdaad93576035755bde2
MatyasD <103099594+MatyasD@users.noreply.github.com>
2023-05-06 20:05:37 +0200

    Add files via upload

---
+++ ui.js
@@ -0,0 +1,8 @@
+
+const menu = document.querySelector(".menu")
+const btnHrat = document.querySelector(".menu-hrat");
+const btnPostavy = document.querySelector(".menu-postavy");
+
+btnPostavy.addEventListener("click", function (){
+    menu.style.display = "none";
+})
