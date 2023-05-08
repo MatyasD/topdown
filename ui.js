08ba00f11fd4fee529d0bce6404f82261c7237ac
MatyasD <103099594+MatyasD@users.noreply.github.com>
2023-05-08 18:38:03 +0200

    Delete ui.js

--- ui.js
+++
@@ -1,8 +0,0 @@
-
-const menu = document.querySelector(".menu")
-const btnHrat = document.querySelector(".menu-hrat");
-const btnPostavy = document.querySelector(".menu-postavy");
-
-btnPostavy.addEventListener("click", function (){
-    menu.style.display = "none";
-})
